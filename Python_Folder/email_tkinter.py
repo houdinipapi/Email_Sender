f82b087bd25f15914a602c51328a952bc7a9bca6
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-11 12:54:25 +0300

    integrating use of pass code

--- Python_Folder/email_tkinter.py
+++ Python_Folder/email_tkinter.py
@@ -72,7 +72,7 @@ def reset():
 top_label = Label(master, text="Custom Python Email app", font=('Calibri', 15))
 top_label.grid(row=0, sticky=N)
 # --- 1st Row --- #
-instruct_label = Label(master, text="Fill in the form below to send an email", font=('Calibri', 11))
+instruct_label = Label(master, text="Fill in the  form", font=('Calibri', 11))
 instruct_label.grid(row=1, sticky=W, padx=5)
 
 # INPUT FIELDS
