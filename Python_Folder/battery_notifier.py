26ea063c46cd721f44e190d730fe1cbe83baa5c4
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-12 16:18:09 +0300

    if not plugged in

--- Python_Folder/battery_notifier.py
+++ Python_Folder/battery_notifier.py
@@ -29,3 +29,35 @@ if __name__ == "__main__":
                 message="Battery is fully charged. Please plug out the charger.",
                 timeout=5
             )
+
+        else:
+            notification.notify(
+                title="Plugged In",
+                message="Please remove the charger. For better battery life, charge up-to 80%.",
+                timeout=5
+            )
+
+    # Not Plugged in
+    else:
+        percent = battery.percent
+
+        if percent <= 20:
+            notification.notify(
+                title="Battery Reminder",
+                message="Your battery is running low. Plug in the charger.",
+                timeout=5
+            )
+
+        elif percent <= 50:
+            notification.notify(
+                title="Battery Reminder",
+                message=f"Battery is {percent} %.",
+                timeout=5
+            )
+
+        elif percent == 100:
+            notification.notify(
+                title="Battery Reminder",
+                message="Fully Charged.",
+                timeout=5
+            )
