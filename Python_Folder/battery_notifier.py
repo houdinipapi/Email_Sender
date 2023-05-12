9da08511954c851952dacae0f55995e77d28b88e
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-12 16:04:03 +0300

    Notification title

--- Python_Folder/battery_notifier.py
+++ Python_Folder/battery_notifier.py
@@ -4,4 +4,14 @@ import psutil
 
 # Returns a tuple
 battery = psutil.sensors_battery()
+plugged = battery.power_plugged
 
+# Code Description
+if __name__ == "__main__":
+    if plugged:
+        percent = battery.percent
+
+        if percent <= 80:
+            notification.notify(
+                # Notification Title
+            )
